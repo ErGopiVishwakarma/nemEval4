0d1faf3100437f0a438e81d6b7caf6e4ab4f06ad
Gopi vishwakarma <g2468vi@gmail.com>
2023-07-18 10:12:31 +0530

    code added

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const connection = mongoose.connect(`mongodb+srv://gopi:gopialpha@cluster0.ge7c9ga.mongodb.net/social?retryWrites=true&w=majority`)
+const connection = mongoose.connect(`${process.env.DATABASE}`)
 
 module.exports = connection 
